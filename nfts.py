546f56da9d6fb76320869c455d877e4000ade660
Comex936 <cvetlanaabramova505@gmail.com>
2026-08-26 19:59:52 +0300

    Create nfts.py

---
+++ nfts.py
@@ -0,0 +1,89 @@
+NFTS = {
+    "golden_cross": {
+        "name": "Golden Cross",
+        "rarity": "Epic",
+        "price": 1000,
+        "bonus": 2,
+        "file_id": "CAACAgIAAxkBAAMrao8S_jbB7AABhqBpVngj-owmkk3uAALyfQACT9gQS7emzD5kM6JhPQQ",
+    },
+
+    "bank_hearts": {
+        "name": "Bank Hearts",
+        "rarity": "Epic",
+        "price": 1500,
+        "bonus": 3,
+        "file_id": "CAACAgIAAxkBAAMtao8UDun_eA-qGIUitx9AFHRbDWMAAuJ2AAKoOHlJmdhTQD3ATO89BA",
+    },
+
+    "keyboard_star": {
+        "name": "Keyboard Star",
+        "rarity": "Secret",
+        "price": 5000,
+        "bonus": 10,
+        "file_id": "CAACAgIAAxkBAAMZao8N7JebB15osrwamRmAw2aZTIAAAkV7AAKiRwlLlHMk5NGD92A9BA",
+    },
+
+    "keyboard_heart": {
+        "name": "Keyboard Heart",
+        "rarity": "Secret",
+        "price": 5500,
+        "bonus": 11,
+        "file_id": "CAACAgIAAxkBAAMLao8MXsQgktlPGNMkk4wOnWqVf-kAAvp7AAKThRBLAAGivlnFZcsSPQQ",
+    },
+
+    "keyboard_f": {
+        "name": "Keyboard F",
+        "rarity": "Secret",
+        "price": 6000,
+        "bonus": 12,
+        "file_id": "CAACAgIAAxkBAAMPao8McUE-fPtdW9H6cDZgwUWkmXcAAi15AAL7vhFLHR8ON5a8too9BA",
+    },
+
+    "ufc_box": {
+        "name": "UFC Box",
+        "rarity": "Secret",
+        "price": 6500,
+        "bonus": 13,
+        "file_id": "CAACAgIAAxkBAAMTao8MdwwijFrDrU-S3JQJ4ePcTrgAAuaJAAKzQMBJK9DxMpuDlYk9BA",
+    },
+
+    "pavel_durov": {
+        "name": "Павел Дуров",
+        "rarity": "Secret",
+        "price": 7000,
+        "bonus": 14,
+        "file_id": "CAACAgIAAxkBAAMHao8MRdvXKGJsvcD8hm9jZ62-KXYAAplpAAK8cwhLntY9EI1KyIo9BA",
+    },
+
+    "polyhedral_cube": {
+        "name": "Polyhedral Cube",
+        "rarity": "Secret",
+        "price": 7500,
+        "bonus": 15,
+        "file_id": "CAACAgIAAxkBAAMjao8RnSwH8fxNIsdJuf1_cCmakigAAiaGAANACUuI5r3ULJSnZD0E",
+    },
+
+    "chinese_soup": {
+        "name": "Chinese Soup",
+        "rarity": "Secret",
+        "price": 8000,
+        "bonus": 16,
+        "file_id": "CAACAgIAAxkBAAMpao8SEevYrzM1kXddi_Tr3wlUzx4AAuhvAAIIlAlL0ar0L-OeLjM9BA",
+    },
+
+    "bank_ducks": {
+        "name": "Bank Ducks",
+        "rarity": "Secret",
+        "price": 8500,
+        "bonus": 17,
+        "file_id": "CAACAgIAAxkBAAMvao8UOZk0vchENbOjO1lFdX72AuwAAh6HAAK_EHlJLh6FFQTa-389BA",
+    },
+
+    "artifact_bank": {
+        "name": "Artifact Bank",
+        "rarity": "OG",
+        "price": None,
+        "bonus": 50,
+        "file_id": "CAACAgIAAxkBAAM1ao8UXDUKWAW_BePJ89Oua2A2CfEAAmWAAALNSnlJbrm4AqShmeI9BA",
+    },
+}
